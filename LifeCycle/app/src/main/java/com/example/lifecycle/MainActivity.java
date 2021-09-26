ca8775f7daaf0898968aba708ddc50539e32aabb
ashpex <ashpex@pm.me>
2021-09-26 20:55:29 +0700

    Add functions

--- LifeCycle/app/src/main/java/com/example/lifecycle/MainActivity.java
+++ LifeCycle/app/src/main/java/com/example/lifecycle/MainActivity.java
@@ -3,17 +3,28 @@ package com.example.lifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
     private EditText edit_text;
+    private TextView text_preview;
     private ConstraintLayout myScreen;
+    private String PREFNAME = "myPrefFile1";
+    private String chosenColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +33,10 @@ public class MainActivity extends AppCompatActivity {
 
         edit_text = (EditText)findViewById(R.id.edit_text);
         button = (Button)findViewById(R.id.exit_button);
+        text_preview = (TextView)findViewById(R.id.text_preview);
         myScreen = (ConstraintLayout)findViewById(R.id.myScreen);
 
-        Toast.makeText(this,"onCreate", Toast.LENGTH_SHORT).show();
+
 
         // Exit button
         button.setOnClickListener(new View.OnClickListener() {
@@ -36,8 +48,36 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // observe text changes made to editText box
+        edit_text.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            // Set background color
+            @Override
+            public void afterTextChanged(Editable editable) {
+                chosenColor = editable.toString().toLowerCase(Locale.US);
+                text_preview.setText(chosenColor);
+                if(chosenColor.length() > 0){
+                    setBackgroundColor(chosenColor,myScreen);
+                }
+
+            }
+        });
+
+
+        Toast.makeText(this,"onCreate", Toast.LENGTH_SHORT).show();
+
 
     }
+
+
 
     @Override
     protected void onStart() {
@@ -48,7 +88,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onPause() {
+
         super.onPause();
+        saveData(chosenColor);
         Toast.makeText(this,"onPause",Toast.LENGTH_SHORT).show();
     }
 
@@ -56,6 +98,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
 
         super.onResume();
+        restoreSavedData();
         Toast.makeText(this,"onResume",Toast.LENGTH_SHORT).show();
     }
 
@@ -72,4 +115,38 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         Toast.makeText(this,"onDestroy",Toast.LENGTH_SHORT).show();
     }
+
+    // Set background color
+    private void setBackgroundColor(String chosenColor, ConstraintLayout myScreen){
+        if(chosenColor.contains("vy")) myScreen.setBackgroundColor(0xff00ff00);
+        if(chosenColor.contains("quan")) myScreen.setBackgroundColor(0xff120629);
+        if(chosenColor.contains("nguyen")) myScreen.setBackgroundColor(0xff120605);
+        if(chosenColor.contains("phuoc")) myScreen.setBackgroundColor(0xff120624);
+        if(chosenColor.contains("vuong")) myScreen.setBackgroundColor(0xff120728);
+
+    }
+
+    // Save temporary data in memory
+    private void saveData(String chosenColor){
+        SharedPreferences myPrefContainer = getSharedPreferences(PREFNAME, Activity.MODE_PRIVATE);
+
+        SharedPreferences.Editor myPrefEditor  = myPrefContainer.edit();
+        String key = "chosenBackgroundColor", value = text_preview.getText().toString();
+        myPrefEditor.putString(key,value);
+        myPrefEditor.commit();
+
+    }
+
+    // Restored saved data on resume
+    private void restoreSavedData(){
+        SharedPreferences myPrefContainer = getSharedPreferences(PREFNAME, Activity.MODE_PRIVATE);
+        String key = "chosenBackgroundColor";
+        String defaultValue = "white";
+        if((myPrefContainer != null) && myPrefContainer.contains(key)){
+            String color = myPrefContainer.getString(key,defaultValue);
+            edit_text.setText(color);
+            setBackgroundColor(color,myScreen);
+        }
+    }
 }
+
