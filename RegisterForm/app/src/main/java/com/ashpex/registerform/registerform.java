b75e112cc4637d815774a45caadc25ce9bb69de3
ashpex <ashpex@pm.me>
2021-10-01 09:37:30 +0700

    refactor

---
+++ RegisterForm/app/src/main/java/com/ashpex/registerform/registerform.java
@@ -0,0 +1,35 @@
+package com.ashpex.registerform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+public class registerform extends AppCompatActivity {
+
+    private EditText txtUsername;
+    private EditText txtPassword;
+    private EditText txtRetype;
+    private RadioGroup radGender;
+    private CheckBox checkBox;
+    private Button btnCalendar;
+    private Button btnReset;
+    private Button btnSignup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registerform);
+
+        txtUsername = (EditText) findViewById(R.id.txtUsername);
+        txtPassword = (EditText) findViewById(R.id.txtPassword);
+        txtRetype = (EditText) findViewById(R.id.txtRetype);
+        radGender = (RadioGroup) findViewById(R.id.radGender);
+        btnCalendar = (Button) findViewById(R.id.btnCalendar);
+        btnReset = (Button) findViewById(R.id.btnReset);
+        btnSignup = (Button) findViewById(R.id.btnSignup);
+    }
+}
